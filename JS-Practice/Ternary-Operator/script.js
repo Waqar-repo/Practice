462e08ca8625d4c112d01836c20e79d2572e139c
Waqar Ahmad <vizzahmadgmail.com>
2026-01-09 20:44:03 +0000

    praactice

---
+++ JS-Practice/Ternary-Operator/script.js
@@ -0,0 +1,25 @@
+// const userName = 5 > 2 ? 'waqar' : 'ahmad';
+
+// const gender = 'f';
+// const userMessage = `${
+//   gender.toUpperCase() === 'F' ? 'She' : 'He'} is college student`;
+
+// console.log(userMessage);
+
+// const result = 0 ? 'Waqar' : 'Ahmad' ? '12' : 0
+// console.log(result);
+
+// function test (t){
+// const name = t ? t.name : 'Please enter your name'
+// return name
+// }
+// console.log(test({name:'waqar'}));
+
+const user = { name: '' };
+
+const msg = user.name ? user.name : user.age ? user.age : 'Guest';
+
+console.log(msg);
+const waqar = 'ahmad'
+const last = "zeeshan"
+let age = 34
